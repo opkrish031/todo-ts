b1f98d4ad4955aaca21133b8828e63c6866c8532
pratyaksh-capsitech <pratyaksh.jangid@capsitech.com>
2026-03-25 19:37:27 +0530

    removed the local storage logic and implemented the redux persist and encrypted the data which is getting store in localstorage using redux persist

---
+++ src/notes/notes.ts
@@ -0,0 +1,2 @@
+// stict mode, redux persist, nuqs, tanstack query, enums,    
+// redux persist clear ls
--- src/redux/slices/todoSlice.ts
+++ src/redux/slices/todoSlice.ts
@@ -1,36 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { Todo } from "../../Interface";
-import { getItems, setItems } from "../../utils/LocalStorage";
+// import { getItems, setItems } from "../../utils/LocalStorage";
 
 const todoSlice = createSlice({
-    name :'todos',
-    initialState: {
-        todos: getItems() as Todo[],
+  name: "todos",
+  initialState: {
+    todos: [] as Todo[],
+  },
+  reducers: {
+    addTodo(state, action: { payload: Todo }) {
+      state.todos = [...state.todos, action.payload];
+      // state.todos.push(action.payload);
     },
-    reducers:{
-        addTodo (state, action :{ payload: Todo }){
-                state.todos = [...state.todos, action.payload];
-                setItems(state.todos);
-        },
-        updateTodo (state, action :{ payload: Todo }){
-            const idx = state.todos.findIndex(todo => todo.id === action.payload.id);
-            state.todos[idx] = {...state.todos[idx], ...action.payload};
-            setItems(state.todos);
-        },
-        deleteTodo (state, action :{payload: string}){
-            state.todos = state.todos.filter(todo => todo.id !== action.payload);
-            setItems(state.todos);
-        },
-        completedTodo (state, action :{payload: string}){
-            const idx = state.todos.findIndex(todo => todo.id === action.payload);
-            // const todoWhichCompleted = state.todos.splice(idx, 1)[0];
-            state.todos[idx].completed = !state.todos[idx].completed;
-            setItems(state.todos);
-        }
+    updateTodo(state, action: { payload: Todo }) {
+      const idx = state.todos.findIndex(
+        (todo) => todo.id === action.payload.id,
+      );
+      state.todos[idx] = { ...state.todos[idx], ...action.payload };
+      // setItems(state.todos);
+    },
+    deleteTodo(state, action: { payload: string }) {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+      // setItems(state.todos);
+    },
+    completedTodo(state, action: { payload: string }) {
+      const idx = state.todos.findIndex((todo) => todo.id === action.payload);
+      // const todoWhichCompleted = state.todos.splice(idx, 1)[0];
+      state.todos[idx].completed = !state.todos[idx].completed;
+      // setItems(state.todos);
+    },
+  },
+});
 
-
-    }
-})
-
-export const { addTodo, deleteTodo, completedTodo, updateTodo } = todoSlice.actions
-export default todoSlice.reducer
+export const { addTodo, deleteTodo, completedTodo, updateTodo } =
+  todoSlice.actions;
+export default todoSlice.reducer;
